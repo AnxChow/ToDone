f666e6ed92436f2033a80cd23a567ccd84742beb
AnxChow <ankitachowdhry97@gmail.com>
2017-02-13 13:04:57 -0600

    finishing touches for P1

--- js/index.js
+++ js/index.js
@@ -29,8 +29,19 @@ window.onload = function() {
 
         var divi=document.createElement("div");
         divi.className='well col-block';
+        var days=currtasks[tasknum].time[0]
+        var hours=currtasks[tasknum].time[1];
+        var mins = currtasks[tasknum].time[2];
+        // if (tasknum==0){
+        //   var head=document.createElement("div");
+        //   head.className='well col-block';
+        //   var node=document.createTextNode("Not Started");
+        //   head.appendChild(node);
+        //   taskbody.appendChild(head);
+        // }
         //var str=currtasks[tasknum].name + 'Expected Time: ' + currtasks[tasknum].time + 'Tag: ' +currtasks[tasknum].tags;
-        var str='<div class="row"><div class="col-md-4">'+currtasks[tasknum].name+'</div><div class="col-md-3">Expected Time: ' +currtasks[tasknum].time+'</div><div class="col-md-3">Tag: '+currtasks[tasknum].tags+'</div></div>';
+        // var str='<div class="row"><div class="col-md-6"><h3>'+currtasks[tasknum].name+'</h3></div><div class="col-md-2"><h5>Expected Time: ' +days+' day(s) '+hours+' hour(s) '+mins+' minute(s)</h5></div><div class="col-md-2"><h5>Tag: '+currtasks[tasknum].tags+'</h5></div><div class="col-md-2"><button class="btn-block">Start Task</div></div>';
+        var str='<div class="row"><div class="col-md-6"><h3>'+currtasks[tasknum].name+'</h3></div><div class="col-md-2"><h5>Expected Time: ' +currtasks[tasknum].time+'</h5></div><div class="col-md-2"><h5>Tag: '+currtasks[tasknum].tags+'</h5></div><div class="col-md-2"><button class="btn-block">Start Task</div></div>'
         //var node=document.createTextNode(str);
         //divi.appendChild(node);
         divi.innerHTML+=str;
